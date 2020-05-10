ba5adf3d92a880e92e3f928979490e19da4c36f0
lfb17 <lfb17@hotmail.com>
2020-05-10 10:37:00 -0500

    since dell update class1

--- src/project2/class1.java
+++ src/project2/class1.java
@@ -5,5 +5,9 @@ public class class1 {
 	public static void main(String[] args) {
 		System.out.println("Test git 3");
 	}
+	
+	public void new(){
+		
+	}
 
 }
--- src/project2/class3.java
+++ src/project2/class3.java
@@ -2,6 +2,6 @@ package project2;
 
 public class class3 {
 	public class3(){
-		
+
 	}
 }
