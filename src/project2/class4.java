c245f2927317cefd9e14b93c267e7ae45cc7c90c
luisfben <luisfben@localhost>
2020-05-10 10:07:47 -0500

    other type data

--- src/project2/class4.java
+++ src/project2/class4.java
@@ -2,6 +2,10 @@ package project2;
 
 public class class4 {
 	public class4(){
+<<<<<<< HEAD
 		String a = new String();
+=======
+		int a;
+>>>>>>> branch 'dev' of https://github.com/Lfb17/project2.git
 	}
 }
