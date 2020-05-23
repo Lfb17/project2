efecd33250b47a385a406a37c9ce198c4ff8e532
luisfben <luisfben@localhost>
2020-05-23 16:50:04 -0500

    merge with stage

--- src/project2/class1.java
+++ src/project2/class1.java
@@ -3,7 +3,7 @@ package project2;
 public class class1 {
 
 	public static void main(String[] args) {
-		System.out.println("Test git 3");
+		System.out.println("Test git 3, add, others, news");
 	}
 
 }
